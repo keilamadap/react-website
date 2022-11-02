bccda046d0dd46b2d199fef32ada260263b295b8
Keila <keila.amadap@hotmail.com>
2022-11-01 23:23:15 -0300

    deploy

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,13 +13,13 @@ const Cards = () => {
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
-              src="../images/verao.jpg"
+              src="https://github.com/keilamadap/react-website/blob/master/public/images/verao.jpg?raw=true"
               text="Praias paradisíacas com piscinas naturais para um dia inesquecível em Natal-RN"
               label="Aventura"
               path="/services"
             />
             <CardItem
-              src="../images/praia_amor.jpg"
+              src="https://github.com/keilamadap/react-website/blob/master/public/images/praia_amor.jpg?raw=true"
               text="Viaje para a Praia do Amor localizada em Pipa-RN"
               label="Aventura"
               path="/services"
@@ -27,19 +27,19 @@ const Cards = () => {
           </ul>
           <ul className="cards__items">
             <CardItem
-              src="../images/forte_reis.jpg"
+              src="https://github.com/keilamadap/react-website/blob/master/public/images/forte_reis.jpg?raw=true"
               text="O Forte dos Reis Magos conta um pouco da história de Natal, localizado na Praia do Meio é uma das maiores atrações turísticas da cidade"
               label="Cultura"
               path="/services"
             />
             <CardItem
-              src="../images/buggy.jpg"
+              src="https://github.com/keilamadap/react-website/blob/master/public/images/buggy.jpg?raw=true"
               text="Com emoção ou sem emoção? Conheça os passeios de buggy que tornam sua estadia inesquecível e radical"
               label="Aventura"
               path="/services"
             />
             <CardItem
-              src="../images/cajueiro.jpg"
+              src="https://github.com/keilamadap/react-website/blob/master/public/images/cajueiro.jpg?raw=true"
               text="O maior cajueiro do MUNDO fica localizado em Pirangi ao norte de Natal"
               label="Natureza"
               path="/services"
